91aaf69cad7eda28897617da59f9da104e0f39f7
Heba-Webdev23 <jtvonline@yahoo.com>
2025-05-19 14:49:30 +0300

    feat: write complex queries with joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,7 +7,8 @@ INNER JOIN users ON bookings.user_id = users.id;
 -- Write a query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 
 SELECT * FROM properties
-LEFT JOIN reviews ON reviews.property_id = properties.id;
+LEFT JOIN reviews ON reviews.property_id = properties.id
+ORDER BY properties.id;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 
